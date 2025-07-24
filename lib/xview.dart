4e0945930df455228414d1923d426c71099380ff
fiekzz <fikrichuck@gmail.com>
2025-07-24 16:27:10 +0800

    feat: expose xview widget container api

--- lib/xview.dart
+++ lib/xview.dart
@@ -1,3 +1,4 @@
 export 'src/components/xview_controller.dart';
 export 'src/components/xview_navigation_history.dart';
 export 'src/components/xview_api.dart';
+export 'src/widgets/xview_widget.dart';
